591b0b2ee7ea565bf6b7a6da340d844fb2c22f12
Flora Lopez <f.lopez@revolentgroup.com>
2022-07-12 16:05:26 -0400

    chld to parent communication

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,5 +1,16 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
+ itemSelected
  @api inputItem
+
+ fireSelectEvent(event) {
+    this.itemSelected = event.target.label
+    console.log(event.target.label)
+    const getEvent = new CustomEvent("inputitemselect", {
+        detail: this.itemSelected
+    })
+    console.log("input item",this.itemSelected)
+    this.dispatchEvent(getEvent)
+ }
 }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,8 +1,10 @@
 import { LightningElement, track } from 'lwc';
 
 export default class ParentComponent extends LightningElement {
-    @track name
+    name
     selected = false
+    searchValue
+    inputItem
     userInput =""
     list= [
         {   id: 1,
@@ -10,17 +12,23 @@ export default class ParentComponent extends LightningElement {
         {    id: 2,
             element: "mown the lawn"}
     ]
-    addInputToList(event) {
-        this.selected = true
+    addInputToList() { 
         let inp=this.template.querySelector("lightning-input");
-        this.name=inp.value;
-        this.list = [
-            ...this.list,
-            {
-                id:this.list.length +1,
-                element: this.name
-            }
-        ]
-    }
-    
+        if (inp.value){
+            this.name=inp.value;
+            this.list = [
+                ...this.list,
+                {
+                    id:this.list.length +1,
+                    element: this.name
+                }
+            ]
+        }
+    } 
+    handleSelect(event) {
+        this.selected = true
+        console.log("selected in handler", this.selected)
+        this.searchValue = event.detail
+        console.log("searchValue" ,this.searchValue )
+    } 
 }
